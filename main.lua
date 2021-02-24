d93359211d658f9e4f51b224ccf21f02f1bb233b
flaribbit <1149761294@qq.com>
2021-02-24 23:25:32 +0800

    update: slash

--- main.lua
+++ main.lua
@@ -6,6 +6,10 @@ local www = {
         self.url = self.url.."."..s
         return self
     end,
+    __div = function(self, s)
+        self.url = self.url.."/"..s
+        return self
+    end,
     get = function(self, fun)
         local b, s, h = http.request(self.url)
         fun({body=b, status=s, header=h})
@@ -13,7 +17,12 @@ local www = {
     end
 }
 setmetatable(www, www)
+setmetatable(_G, {__index = function(self, k) return k end})
 
 www.baidu.com:get(function (res)
+    print(res.status, #res.body)
+end)
+
+;(www.test.com/test):get(function (res)
     print(res.status, #res.body)
 end)
